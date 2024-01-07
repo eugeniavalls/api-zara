58566aa0af9098b8e461c34a4be11fd1ee9e9d91
eugeniavalls <e.vallsalfaro@gmail.com>
2024-01-07 14:37:30 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+console.clear()
+
+// --------------------
+// HACER EL CONTROLLER ROUTER ETC ETC
+// Los modelos en su carpeta, el schema tambien 
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
+
+let URL_ATLAS = process.env.URL_ATLAS || 'mongodb://127.0.0.1:27017/zarapp'
+
+const app = express()
+
+const conectar = async () => await mongoose.connect(URL_ATLAS)
+    .then(()=> console.log('Conectado a BBDD'))
+    .catch(error => console.log(error))
+
+conectar()
+
+const usuarioSchema = new mongoose.Schema(
+    {nombre : String , pass : String},
+    {collection : 'usuarios'}
+)
+
+const Usuario = mongoose.model('Usuario', usuarioSchema)
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+app.get('/', async (req, res, next)=>{
+    // Aqui flatarian los middleware router etc
+    const buscar = await Usuario.find()
+    res.json(buscar)
+})
+
+app.post('/', async (req, res, next) => {
+    const {nombre, pass} = req.body
+    const buscar = await Usuario.findOne({
+        nombre, pass
+    })
+    res.json(buscar)
+})
+
+app.listen(3000, ()=> console.log('Iniciando API'))
